82d550da1c6418557b279bd04e4e9f12489c9f6f
Netanel <mordakhay.netanel@gmail.com>
2024-10-18 17:38:18 +0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Grid
-        templateAreas={`"header header"
-                        "user computer"
-                        "footer footer"`}
-        gridTemplateRows={"150px 1fr 50px"}
-        gridTemplateColumns={"repeat(2, 1fr)"}
+        templateAreas={{
+          base: `"header"
+                  "info"
+                  "user"
+                  "computer"
+                  "footer"`,
+          lg: `"header header"
+                "info info"
+                "user computer"
+                "footer footer"`,
+        }}
+        gridTemplateRows={{
+          base: "80px 0.6fr 1fr 1fr 25px",
+          lg: "150px 150px 1fr 50px",
+        }}
+        gridTemplateColumns={{ base: '"repeat(2, 1fr)"', lg: "repeat(2, 1fr)" }}
+        minHeight="100vh"
+        //maxWidth="1024px"
+        //margin="0 auto"
       >
-        <GridItem bg="blue" area={"header"}>
-          header
+        <GridItem area={"header"} className="centered-content">
+          <Header />
         </GridItem>
-        <GridItem bg="orange" area={"user"}>
+        <GridItem bg="gray" area={"info"} className="centered-content">
+          Info
+        </GridItem>
+        <GridItem bg="orange" area={"user"} className="centered-content">
           user
         </GridItem>
-        <GridItem bg="yellow" area={"computer"}>
+        <GridItem bg="yellow" area={"computer"} className="centered-content">
           computer
         </GridItem>
-        <GridItem bg="gray" area={"footer"}>
+        <GridItem bg="gray" area={"footer"} className="centered-content">
           footer
         </GridItem>
       </Grid>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Heading, HStack } from "@chakra-ui/react";
+import { FaHandScissors } from "react-icons/fa";
+import "animate.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HStack className="header-container animate__animated animate__fadeInDown">
+      <FaHandScissors className="logo-icon" />
+      <Heading as="h1" size="xl">
+        Rock Paper Scissors
+      </Heading>
+    </HStack>
+  );
 };
 
 export default Header;
