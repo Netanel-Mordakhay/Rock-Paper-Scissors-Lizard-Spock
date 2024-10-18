aea5c0735631480e70b59baed37cd0e867c43e11
Netanel <mordakhay.netanel@gmail.com>
2024-10-19 01:33:58 +0300

    cards hover animation

--- src/components/PlayerInfo.tsx
+++ src/components/PlayerInfo.tsx
@@ -9,7 +9,11 @@ interface Props {
 
 const PlayerInfo = ({ title, currentChoice, currentScore }: Props) => {
   return (
-    <Stack alignItems="center" marginX={{ base: "2", lg: "10" }}>
+    <Stack
+      alignItems="center"
+      marginX={{ base: "2", lg: "10" }}
+      className="scale-animation"
+    >
       <Heading as="h3" size="lg">
         {title}
       </Heading>
